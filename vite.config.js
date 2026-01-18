1da00165a7a2c05b16360ea692c56ddd56c576b6
Filip Vitas <filip.vitas.dev@gmail.com>
2026-01-18 02:30:03 +0100

    feat: update vite config to support website and add website html and css

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { crx } from '@crxjs/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 // import react from '@vitejs/plugin-react'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 import { resolve } from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import chromeManifest from './manifest.chrome.json'
@@ -11,11 +12,22 @@ import { version } from './package.json'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const manifest = env.VITE_EXTENSION === 'FIREFOX' ? firefoxManifest : chromeManifest
-  const outputDir = env.VITE_EXTENSION === 'FIREFOX' ? 'dist/firefox' : 'dist/chrome'
   const browser = env.VITE_EXTENSION === 'FIREFOX' ? 'firefox' : 'chrome'
 
+  const isWebsite = mode === 'website'
+  const extensionDir = env.VITE_EXTENSION === 'FIREFOX' ? 'dist/firefox' : 'dist/chrome'
+  const outputDir = isWebsite ? 'dist/website' : extensionDir
+
+  console.log()
   return {
-    plugins: [react(), tailwindcss(), crx({ manifest, browser: browser })],
+    // root: isWebsite ? resolve(__dirname, 'website') : __dirname,
+    root: isWebsite ? resolve(__dirname, 'website') : __dirname,
+    plugins: [
+      codeInspectorPlugin({ bundler: 'vite', editor: 'webstorm', hotKeys: ['altKey'] }),
+      react(),
+      tailwindcss(),
+      ...(!isWebsite ? [crx({ manifest, browser })] : []),
+    ],
     resolve: {
       alias: {
         '@': `${resolve(__dirname, 'src')}`,
@@ -24,17 +36,28 @@ export default defineConfig(({ mode }) => {
     define: {
       'import.meta.env.APP_VERSION': JSON.stringify(version),
     },
-    build: {
-      outDir: outputDir,
-      sourcemap: false,
-      emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom', 'react-dom/client'],
+    build: isWebsite
+      ? {
+          outDir: resolve(__dirname, 'dist/website'),
+          sourcemap: false,
+          emptyOutDir: true,
+          rollupOptions: {
+            input: {
+              website: resolve(__dirname, 'website/index.html'),
+            },
+          },
+        }
+      : {
+          outDir: outputDir,
+          sourcemap: false,
+          emptyOutDir: true,
+          rollupOptions: {
+            output: {
+              manualChunks: {
+                vendor: ['react', 'react-dom', 'react-dom/client'],
+              },
+            },
           },
         },
-      },
-    },
   }
 })
